bc39941d44ac78d4c3a4a9b6732f229061f9d3c6
Gise01 <arenagise1@gmail.com>
2021-09-24 12:39:14 -0300

    NavBar

---
+++ zhapp/src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+
+const NavBar = () => {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="secondary" variant="dark">
+  <Container>
+  <Navbar.Brand href="#home">ZeroHumedad</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="me-auto">
+      <Nav.Link href="#productos">Productos</Nav.Link>
+      <Nav.Link href="#cotizador">Cotizador</Nav.Link>
+      <Nav.Link href="#contacto">Contacto</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
+  )
+}
+
+export default NavBar
